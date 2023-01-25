3ad2a5d25c15f93d98e3e591d4b5da53d3313c00
JP <94187003+SquareRo0t@users.noreply.github.com>
2023-01-25 21:00:46 +0100

    Update KeyEncDec.java

--- src/main/java/Library/KeyEncDec.java
+++ src/main/java/Library/KeyEncDec.java
@@ -7,13 +7,20 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
-    //The method takes in a string and returns a key object. 
-    //The string is decoded using the Base64 class and then converted to a key object.
+    // The method takes in a string and returns a key object. 
+    // The string is decoded using the Base64 class and then converted to a key object.
 public class KeyEncDec {
+    // The code starts by declaring the variables that are needed for decoding and then declares a method 
+    // called decodeKeyBytesPublic().
+    // This method takes in a String, which contains the encoded public key,
+    //  and returns a byte array of the decoded public key.
     public static Key decodeKeyBytesPublic(String encodedPublicKey){
         byte[] keyBytes = Base64.getDecoder().decode(encodedPublicKey);
         try {
+    // The next step is to create an instance of KeyFactory using "RSA" as its algorithm name.
             final KeyFactory fact = KeyFactory.getInstance("RSA");
+    // Then it creates an X509EncodedKeySpec object with the bytes contained 
+    // in the byte array returned by decodeKeyBytesPublic() as its input parameter.
             final X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(keyBytes);
             return fact.generatePublic(pubKeySpec);
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
@@ -21,8 +28,8 @@ public class KeyEncDec {
             throw new Error("Failed to read PoaOnboarding public key");
         }
     }
-    //The method takes in a key object and returns a string. 
-    //The key is encoded using the Base64 class and then converted to a string.
+    // The method takes in a key object and returns a string. 
+    // The key is encoded using the Base64 class and then converted to a string.
     public static String stringEncodedKey(Key publicKey){
         return Base64.getEncoder().encodeToString(publicKey.getEncoded());
     }
