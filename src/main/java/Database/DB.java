cd25a7efe666b29494671fd26655b8af1ea73c6f
Dayrren <isafor-0@student.ltu.se>
2023-02-03 16:38:37 +0100

    Added a few more getters and added some Jdocks (be aware some of these are untested) alto its mostly reused code

--- src/main/java/Database/DB.java
+++ src/main/java/Database/DB.java
@@ -10,6 +10,14 @@ public class DB {
 
     public static String table = "key_db.keys";
 
+    /**
+     * Attempts to connect to a localhost mysql db if that fails
+     * <p>
+     * Attempt to connect to the a docker db (this is in the case the application is running in a docker container)
+     * <p>
+     * @return Connection
+     * @author Isak Forsgren
+     */
     public static Connection Connect(){
         Connection conn = null;
 
@@ -41,26 +49,29 @@ public class DB {
     }
 
     /**Creates a table in the database with the columns
-     *
-     * @apiNote Name      String,
-     *          Priv_key  String,
-     *          Pub_key   String,
-     *          ip        String,
-     *          port      Int
-     *
+     *<blockquote><pre>
+     *Name      String  Index 1
+     *Priv_key  String  Index 2
+     *Pub_key   String  Index 3
+     *ip        String  Index 4
+     *port      Int     Index 5
+     *</pre></blockquote><p>
+     * Where Name is the primary key, and all the columns have the NOT NULL attribute
+     * @author Isak Forsgren
      */
 
     public static void createTable() {
 
         Connection c = DB.Connect();
 
-        String sql = "CREATE TABLE IF NOT EXISTS " + table + "(\n" +
-                "  Name VARCHAR(20) NOT NULL,\n" +
-                "  Priv_key TEXT NOT NULL,\n" +
-                "  Pub_key TEXT NOT NULL,\n" +
-                "  ip VARCHAR(32) NOT NULL,\n" +
-                "  port INT(11) UNSIGNED NOT NULL,\n" +
-                "  PRIMARY KEY(`Name`(20)))\n";
+        String sql = "CREATE TABLE IF NOT EXISTS "
+                   + table
+                   + "  (Name VARCHAR(20) NOT NULL,"
+                   + "  Priv_key TEXT NOT NULL,"
+                   + "  Pub_key TEXT NOT NULL,"
+                   + "  ip VARCHAR(32) NOT NULL,"
+                   + "  port INT(11) UNSIGNED NOT NULL,"
+                   + "  PRIMARY KEY(`Name`(20)))";
 
         System.out.println(sql);
         try {
--- src/main/java/Database/Getters.java
+++ src/main/java/Database/Getters.java
@@ -7,6 +7,11 @@ import java.sql.*;
 
 public class Getters {
 
+    /**
+     * @param Name The primary key in the database
+     * @return Key
+     * @author Isak Forsgren
+     */
     public static Key getPriv(String Name){
 
         String resString = null;
@@ -30,15 +35,17 @@ public class Getters {
             preparedStmt.close();
             c.close();
 
-            //temp
-            System.out.println(resString);
-
         }catch (SQLException e){
             System.out.println(e.getErrorCode());
         }
         return KeyEncDec.decodeKeyBytesPrivate(resString);
     }
 
+    /**
+     * @param Name The primary key in the database
+     * @return Key
+     * @author Isak Forsgren
+     */
     public static Key getPub(String Name){
 
         String resString = null;
@@ -69,5 +76,76 @@ public class Getters {
         return KeyEncDec.decodeKeyBytesPublic(resString);
     }
 
+    /**
+     * untested fuction did not have accses to db
+     * @param Name The primary key in the database
+     * @return ip
+     * @author Isak Forsgren
+     */
+
+    public static String getIp(String Name){
+
+        String resString = null;
+
+        try {
+            Connection c = DB.Connect();
+
+            String sql = "SELECT * FROM " + DB.table + " WHERE Name = ?";
+
+            PreparedStatement preparedStmt = c.prepareStatement(sql);
+            preparedStmt.setString(1,Name);
+
+            ResultSet res = preparedStmt.executeQuery();
+
+            //goes trew the entire result set and stores them to a varible
+            //in this case its only 1
+            while(res.next())
+                resString = res.getString(4);
+
+            res.close();
+            preparedStmt.close();
+            c.close();
+
+        }catch (SQLException e){
+            System.out.println(e.getErrorCode());
+        }
+        return resString;
+    }
+
+    /**
+     * untested fuction did not have accses to db
+     * @param Name The primary key in the database
+     * @return port
+     * @author Isak Forsgren
+     */
+
+    public static String getPort(String Name){
+
+        String resString = null;
+
+        try {
+            Connection c = DB.Connect();
+
+            String sql = "SELECT * FROM " + DB.table + " WHERE Name = ?";
+
+            PreparedStatement preparedStmt = c.prepareStatement(sql);
+            preparedStmt.setString(1,Name);
+
+            ResultSet res = preparedStmt.executeQuery();
+
+            //goes trew the entire result set and stores them to a varible
+            //in this case its only 1
+            while(res.next())
+                resString = res.getString(5);
+
+            res.close();
+            preparedStmt.close();
+            c.close();
+
+        }catch (SQLException e){
+            System.out.println(e.getErrorCode());
+        }
+        return resString;
+    }
 
 }
