2e6eb12281ab931a431f39839a9b2e0b55d1e82f
Jacob <jacsjs-dev@pm.me>
2023-02-28 11:56:34 +0100

    start implementing incorrect keys test case

--- src/main/java/Application/TestCases/IncorrectKeysTest.java
+++ src/main/java/Application/TestCases/IncorrectKeysTest.java
@@ -1,5 +1,27 @@
 package Application.TestCases;
 
+import Application.Agent;
+
 public class IncorrectKeysTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        Agent agent0 = new Agent(
+                "agent0",
+                0,
+                "localhost",
+                0);
+
+        Agent agent1 = new Agent(
+                "agent1",
+                1,
+                "localhost",
+                1);
+        try {
+            agent1.start();
+        }catch (Exception e){
+            System.out.println("Error when starting agent1: (\"bingo bango det funkar inte ;(\") " + e);
+            System.exit(0);
+        }
+    }
 
 }
