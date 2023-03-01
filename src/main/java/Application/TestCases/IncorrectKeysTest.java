e8d417e3d6c5b70951b34040c01b6b55c2529fd1
Jacob <jacsjs-dev@pm.me>
2023-03-01 11:26:50 +0100

    working on testcase

--- src/main/java/Application/TestCases/IncorrectKeysTest.java
+++ src/main/java/Application/TestCases/IncorrectKeysTest.java
@@ -1,25 +1,39 @@
 package Application.TestCases;
 
+import java.security.KeyPair;
+
 import Application.Agent;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 public class IncorrectKeysTest {
 
     public static void main(String[] args) throws InterruptedException {
+
+        // Generate test keys
+        KeyPair testKeyPair0    = Keys.keyPairFor(SignatureAlgorithm.RS256);
+        KeyPair testKeyPair1    = Keys.keyPairFor(SignatureAlgorithm.RS256);
+
+        // Set keys manually instead of fetching from database
         Agent agent0 = new Agent(
                 "agent0",
                 0,
                 "localhost",
-                0);
+                0,
+                testKeyPair0.getPublic(),
+                testKeyPair0.getPrivate());
 
         Agent agent1 = new Agent(
                 "agent1",
                 1,
                 "localhost",
-                1);
+                1,
+                testKeyPair1.getPublic(),
+                testKeyPair1.getPrivate());
         try {
             agent1.start();
         }catch (Exception e){
-            System.out.println("Error when starting agent1: (\"bingo bango det funkar inte ;(\") " + e);
+            System.out.println("Error when starting agent1" + e);
             System.exit(0);
         }
     }
