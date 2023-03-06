8d30e3d8bcf942d400c44cb7510a3ab77493fc89
Malkolm <amouli-0@student.ltu.se>
2023-03-06 09:01:58 +0100

    incorrectkeys är nu klar

--- src/main/java/Application/TestCases/IncorrectKeysTest.java
+++ src/main/java/Application/TestCases/IncorrectKeysTest.java
@@ -1,8 +1,10 @@
 package Application.TestCases;
 
 import java.security.KeyPair;
+import java.util.Date;
 
 import Application.Agent;
+import Library.PoA;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
@@ -36,6 +38,21 @@ public class IncorrectKeysTest {
             System.out.println("Error when starting agent1" + e);
             System.exit(0);
         }
+
+        String[] metadata = {};
+        Date date =  new Date(System.currentTimeMillis()+ Days(1));
+
+        PoA poa = agent0.setValues(0,
+                3,
+                "agent1",
+                date,
+                metadata);
+
+        // Send the PoA from the agent0
+        agent0.sendPoA(poa, "localhost", 889);
     }
 
+    private static long Days(int i) {
+        return i * 86400000;
+    }
 }
